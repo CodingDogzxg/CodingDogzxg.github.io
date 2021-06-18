3387305b21b3385b489a4d59e5f00a44bb332fcf
CodingDogzxg <2461095263@qq.com>
2021-06-18 18:45:51 +0800

    Fri. 19:00 backup

--- period&comma/corrector.js
+++ period&comma/corrector.js
@@ -3,37 +3,69 @@ const { defineComponent, ref, h } = Vue
 var Main = defineComponent({
     methods: {
         open1() {
+            a = this.textarea;
+            if(this.select_value == "选择语境") {
+                this.$notify({
+                    title: '提示',
+                    message: h('i', {style: 'color: #409EFF'}, '空语境？句读之不知？')
+                    });
+            } else if(!a) {
+                this.$notify({
+                    title: '提示',
+                    message: h('i', {style: 'color: #409EFF'}, '空句读？惑之不解？')
+                    });
+            } else {
+                let tx = this.textarea;
+                let lang = this.select_value;
+                switch (lang) {
+                    case "en" : this.funcReEnglish(tx); break;
+                    case "fr" : this.prompt_unfinish_text("Français"); break;
+                    case "zh" : this.prompt_unfinish_text("中文"); break;
+                    case "jp" : this.prompt_unfinish_text("日本語"); break;
+                    case "mx" : this.prompt_unfinish_text("混合"); break;
+                }
+            }
+        },
+        prompt_unfinish_text(lang) {
             this.$notify({
-            title: '提示',
-            message: h('i', { style: 'color: #409EFF'}, '功能正在制作，客官敬请期待！')
-            });
+                title: '提示',
+                message: h('i', {style: 'color: #409EFF'}, lang + '语境功能尚未完成，客官敬请期待！')
+                });
+        },
+        select_change(value) {
+            console.log(value);
+        },
+        funcReEnglish(text) {
+            reg = /[,.:;!?]/g.exec(text);
+            console.log(reg);
         },
     },
     setup() {
         return {
+            select_value: ref("选择语境"),
             input1: ref(''),
-            textarea: ref('')
+            textarea: ref(''),
         }
     },
     data() {
         return {
             options: [{
-                value: '选项1',
+                value: 'en',
                 label: 'English',
             }, {
-                value: '选项2',
+                value: 'fr',
                 label: 'Français',
             }, {
-                value: '选项3',
+                value: 'zh',
                 label: '中文',
             }, {
-                value: '选项4',
+                value: 'jp',
                 label: '日本語',
             }, {
-                value: '选项5',
+                value: 'mx',
                 label: '混合',
             }],
-            value: 'English'
+            value: this.select_value
         }
     }
 });
